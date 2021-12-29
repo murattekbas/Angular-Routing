e31c27c531f8afa31b439489ad183e044d6303e6
murattekbas <muratekbas@gmail.com>
2021-12-29 12:15:06 +0300

    Route Resolvers

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { MessageService } from '../../messages/message.service';
 
-import { Product } from '../product';
+import { Product, ProductResolved } from '../product';
 import { ProductService } from '../product.service';
 
 @Component({
@@ -18,12 +18,19 @@ export class ProductEditComponent implements OnInit {
 
   constructor(private productService: ProductService,private messageService: MessageService,private route:ActivatedRoute,private router:Router) { }
   ngOnInit(): void {
-    this.route.paramMap.subscribe(
+    /* this.route.paramMap.subscribe(
       params=>{
         const id=+params.get('id');
         this.getProduct(id);
       }
-    )
+    ) */
+    this.route.data.subscribe(data=>{
+      const resolvedData:ProductResolved=data['resolvedData'];
+    this.errorMessage=resolvedData.error;
+    this.onProductRetrieved(resolvedData.product);
+
+    })
+    
   }
 
   getProduct(id: number): void {
