d2663ce6819d724bc5536cba5f944a93e5e59e2b
murattekbas <muratekbas@gmail.com>
2021-12-29 15:17:35 +0300

    Styling,Animating, and Watching Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router,Event,NavigationStart,NavigationEnd,NavigationError,NavigationCancel } from '@angular/router';
 
 import { AuthService } from './user/auth.service';
+import {slideInAnimation} from './app.animation'
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[slideInAnimation]
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
+  loading=true;
 
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
@@ -22,7 +25,19 @@ export class AppComponent {
     return '';
   }
 
-  constructor(private authService: AuthService,private router:Router) { }
+  constructor(private authService: AuthService,private router:Router) { 
+    router.events.subscribe((routerEvent:Event)=>{
+      if (routerEvent instanceof NavigationStart){
+        this.loading=true;
+      }
+      if (routerEvent instanceof NavigationEnd||
+        routerEvent instanceof NavigationCancel||
+        routerEvent instanceof NavigationError){
+          this.loading=false;
+        }
+
+    })
+  }
 
   logOut(): void {
     this.authService.logout();
