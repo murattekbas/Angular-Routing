8bba6b3f78688f529fa2f42dbca892eab34a2be4
murattekbas <muratekbas@gmail.com>
2021-12-30 11:35:00 +0300

    Route Guards

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -14,8 +14,22 @@ export class ProductEditComponent implements OnInit {
   pageTitle = 'Product Edit';
   errorMessage: string;
 
-  product: Product;
+  get isDirty():boolean{
+    return JSON.stringify(this.originalProduct)!==JSON.stringify(this.currentProduct);
+  }
+ 
   private dataIsValid:{[key:string]:boolean}={};
+  private currentProduct:Product;
+  private originalProduct:Product;
+
+  get product():Product{
+    return this.currentProduct;
+  }
+
+  set product(value:Product){
+    this.currentProduct=value;
+    this.originalProduct={...value};
+  }
 
   constructor(private productService: ProductService,private messageService: MessageService,private route:ActivatedRoute,private router:Router) { }
   ngOnInit(): void {
@@ -78,7 +92,11 @@ export class ProductEditComponent implements OnInit {
       Object.keys(this.dataIsValid).every(d => this.dataIsValid[d] === true));
   }
 
- 
+ reset():void{
+   this.dataIsValid=null;
+   this.currentProduct=null;
+   this.originalProduct=null;
+ }
 
   saveProduct(): void {
     if (this.isValid()) {
@@ -102,7 +120,7 @@ export class ProductEditComponent implements OnInit {
     if (message) {
       this.messageService.addMessage(message);
     }
-
+    this.reset();
     // Navigate back to the product list
     this.router.navigate(['/products']);
   }
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -3,18 +3,22 @@ import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductEditGuard } from './product-edit/product-edit.guard';
 
 import { SharedModule } from '../shared/shared.module';
 import {ProductResolver} from './product-resolver.service';
 import  {ProductEditInfoComponent} from './product-edit/product-edit-info.component';
 import  {ProductEditTagsComponent} from './product-edit/product-edit-tags.component';
+import {AuthGuard} from '../user/auth.guard';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
-      {path:'products',
-      children:[
+      {
+        path:'products',
+        canActivate:[AuthGuard],
+        children:[
         {
           path:'',
           component:ProductListComponent
@@ -28,6 +32,7 @@ import  {ProductEditTagsComponent} from './product-edit/product-edit-tags.compon
         {
           path:':id/edit',
           component:ProductEditComponent,
+          canDeactivate:[ProductEditGuard],
           resolve:{resolvedData:ProductResolver},
           children:[
             {
